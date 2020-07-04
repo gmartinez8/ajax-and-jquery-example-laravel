50f1a94f1dbf2d83bf06eebe063aeb49c5aa24af
Gustavo <gustavomartinez1990@gmail.com>
2020-07-04 07:28:15 -0400

    Define route for: Delete product

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::DELETE('/delete-product/{id}', 'HomeController@deleteProduct')->name('deleteProduct');
